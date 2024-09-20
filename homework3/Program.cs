3bd55067fdaaf62aab6b48b7da4707d02e858f71
Luka Bitsadze <bitsadzeluka@gmail.com>
2024-09-20 18:58:38 +0400

    Add project files.

---
+++ homework3/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#1
+            //Console.Write("Enter number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine($"sum is: {sum}");
+
+
+
+
+            //#2
+            //Console.Write("Enter number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= 10; i++) {
+
+            //    Console.WriteLine($"{number}*{i}={number * i}");
+            //}
+
+
+
+
+            //#3
+            //Console.Write("Enter number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            //#4
+            //Console.Write("Enter number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int fact = 1;
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    fact*= i;
+            //}
+            //Console.WriteLine($"factorial of {number} is {fact}");
+
+
+
+            //#5
+            //Console.Write("Enter number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write(i);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            //#6
+            //Console.Write("Enter first number: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter second number: ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //int start=a, end=b;
+            //if (a>b)
+            //{
+            //    start = b;
+            //    end = a;
+            //}
+
+            //for (int i = start; i <= end; i++) {
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            
+            //}
+
+
+        }
+    }
+}
